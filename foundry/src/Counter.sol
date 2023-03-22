27180e639415b07e9fe7a5c94bc2e5b43f1cd9eb
Mikołaj Jakubowski <mikolaj.jakubowski@protonmail.com>
2023-03-22 21:07:14 +0100

    🎸 Install openzeppelin (#3)

--- foundry/src/Counter.sol
+++ foundry/src/Counter.sol
@@ -5,7 +5,11 @@ contract Counter {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
-        number = newNumber;
+        if (newNumber == 100) {
+            number = 420;
+        } else {
+            number = newNumber;
+        }
     }
 
     function increment() public {
